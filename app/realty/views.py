da391e17fa2471e74e41f224b65f56ecc58cf777
InnFil <inna_tretyakova_2015@mail.ru>
2024-03-01 14:51:40 +0400

    M-5: fixed-field-types-and-error handling

--- app/realty/views.py
+++ app/realty/views.py
@@ -3,18 +3,19 @@ from rest_framework import serializers
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from realty.models import Flat
+from django.db.models import ObjectDoesNotExist
 
 
 class FlatListAPI(APIView):
     class FlatSerializer(serializers.Serializer):
-        id = serializers.CharField()
+        id = serializers.IntegerField()
         description = serializers.CharField()
-        photo = serializers.CharField()
-        price = serializers.CharField()
-        square = serializers.CharField()
-        rooms = serializers.CharField()
-        floor = serializers.CharField()
-        number = serializers.CharField()
+        photo = serializers.ImageField()
+        price = serializers.IntegerField()
+        square = serializers.IntegerField()
+        rooms = serializers.IntegerField()
+        floor = serializers.IntegerField()
+        number = serializers.IntegerField()
         status = serializers.CharField()
 
     def get(self, request):
@@ -25,17 +26,20 @@ class FlatListAPI(APIView):
 
 class FlatDetailAPI(APIView):
     class FlatSerializer(serializers.Serializer):
-        id = serializers.CharField()
+        id = serializers.IntegerField()
         description = serializers.CharField()
-        photo = serializers.CharField()
-        price = serializers.CharField()
-        square = serializers.CharField()
-        rooms = serializers.CharField()
-        floor = serializers.CharField()
-        number = serializers.CharField()
+        photo = serializers.ImageField()
+        price = serializers.IntegerField()
+        square = serializers.IntegerField()
+        rooms = serializers.IntegerField()
+        floor = serializers.IntegerField()
+        number = serializers.IntegerField()
         status = serializers.CharField()
 
     def get(self, request, pk):
-        flat = Flat.objects.get(id=pk)
-        data = self.FlatSerializer(flat).data
-        return Response(data)
+        try:
+            flat = Flat.objects.get(id=pk)
+            data = self.FlatSerializer(flat).data
+            return Response(data)
+        except ObjectDoesNotExist:
+            return Response(f"Отсутствует квартира с id равным {pk}")
