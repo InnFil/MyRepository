06bf437d1a402baae55b7ea44a13c3f4e04a9e09
InnFil <inna_tretyakova_2015@mail.ru>
2024-03-01 14:17:20 +0400

    M-5: flats-API

--- app/realty/views.py
+++ app/realty/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import serializers
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from realty.models import Flat
+
+
+class FlatListAPI(APIView):
+    class FlatSerializer(serializers.Serializer):
+        id = serializers.CharField()
+        description = serializers.CharField()
+        photo = serializers.CharField()
+        price = serializers.CharField()
+        square = serializers.CharField()
+        rooms = serializers.CharField()
+        floor = serializers.CharField()
+        number = serializers.CharField()
+        status = serializers.CharField()
+
+    def get(self, request):
+        flats = Flat.objects.all()
+        data = self.FlatSerializer(flats, many=True).data
+        return Response(data)
+
+
+class FlatDetailAPI(APIView):
+    class FlatSerializer(serializers.Serializer):
+        id = serializers.CharField()
+        description = serializers.CharField()
+        photo = serializers.CharField()
+        price = serializers.CharField()
+        square = serializers.CharField()
+        rooms = serializers.CharField()
+        floor = serializers.CharField()
+        number = serializers.CharField()
+        status = serializers.CharField()
+
+    def get(self, request, pk):
+        flat = Flat.objects.get(id=pk)
+        data = self.FlatSerializer(flat).data
+        return Response(data)
