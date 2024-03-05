1420e927b83e080776b85e8348e0c578e2a0b735
InnFil <inna_tretyakova_2015@mail.ru>
2024-03-05 10:29:16 +0400

    M-7: removed the spase

--- app/realty/models.py
+++ app/realty/models.py
@@ -23,7 +23,6 @@ class Flat(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-
     class Meta:
         verbose_name = 'Квартира'
         verbose_name_plural = 'Квартиры'
