d68bc2fc271ba5461acc19c29a87e4a469555084
InnFil <inna_tretyakova_2015@mail.ru>
2024-03-06 15:16:21 +0400

    M-8: Add API for floor

--- app/realty/views.py
+++ app/realty/views.py
@@ -2,7 +2,7 @@
 from rest_framework import serializers
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from realty.models import Flat
+from realty.models import Flat, Floor
 from django.db.models import ObjectDoesNotExist
 
 
@@ -14,7 +14,7 @@ class FlatListAPI(APIView):
         price = serializers.IntegerField()
         square = serializers.IntegerField()
         rooms = serializers.IntegerField()
-        floor = serializers.IntegerField()
+        floor = serializers.IntegerField(source='floor.number')
         number = serializers.IntegerField()
         status = serializers.CharField()
 
@@ -43,3 +43,27 @@ class FlatDetailAPI(APIView):
             return Response(data)
         except ObjectDoesNotExist:
             return Response(f"Отсутствует квартира с id равным {pk}")
+
+
+class FloorInformationAPI(APIView):
+    class FloorSerializer(serializers.Serializer):
+        class FlatSerializer(serializers.Serializer):
+            id = serializers.IntegerField()
+            description = serializers.CharField()
+            photo = serializers.ImageField()
+            price = serializers.IntegerField()
+            square = serializers.IntegerField()
+            rooms = serializers.IntegerField()
+            number = serializers.IntegerField()
+            status = serializers.CharField()
+
+        id = serializers.IntegerField()
+        number = serializers.IntegerField()
+        color = serializers.CharField()
+        lighting = serializers.CharField()
+        flat_set = FlatSerializer(many=True)
+
+    def get(self, request, pk):
+        floors = Floor.objects.get(id=pk)
+        data = self.FloorSerializer(floors).data
+        return Response(data)
