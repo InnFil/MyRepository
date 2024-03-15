966f5db4781280d3a40d6f6c502de9bc75a7d011
InnFil <156078099+InnFil@users.noreply.github.com>
2024-03-15 11:50:30 +0400

    M-15: Add admin for floors (#13)
    * M-15: Add admin for floors
    
    * M-15: Fixed model Flat

--- app/realty/admin.py
+++ app/realty/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from .models import Flat
+from .models import Flat, Floor
 
 admin.site.register(Flat)
+admin.site.register(Floor)
--- app/realty/models.py
+++ app/realty/models.py
@@ -49,6 +49,10 @@ class Floor(models.Model):
     def __str__(self):
         return str(self.number)
 
+    class Meta:
+        verbose_name = 'Этаж'
+        verbose_name_plural = 'Этажи'
+
 
 class Section(models.Model):
     name = models.TextField(verbose_name='Название', max_length=255, unique=True)
