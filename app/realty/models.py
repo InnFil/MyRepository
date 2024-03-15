fb404ae1c1978e8a3fe0cc6b3dafc8ce036a698c
InnFil <inna_tretyakova_2015@mail.ru>
2024-03-15 13:28:48 +0400

    M-16: Add admin for sections

--- app/realty/models.py
+++ app/realty/models.py
@@ -60,6 +60,10 @@ class Section(models.Model):
     def __str__(self):
         return self.name
 
+    class Meta:
+        verbose_name = 'Cекция'
+        verbose_name_plural = 'Секции'
+
 
 class Building(models.Model):
     COMPLETE = 'COMPLETE'
