2256d82051206f70de80ccb241f5d47496624db2
InnFil <156078099+InnFil@users.noreply.github.com>
2024-03-15 13:29:55 +0400

    M-16: Add admin for sections (#14)

--- app/realty/admin.py
+++ app/realty/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Flat, Floor
+from .models import Flat, Floor, Section
 
 admin.site.register(Flat)
 admin.site.register(Floor)
+admin.site.register(Section)
