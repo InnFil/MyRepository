d03e7c0af9ea3af80c1064ec2354b79cb53976ec
InnFil <156078099+InnFil@users.noreply.github.com>
2024-03-18 10:25:54 +0400

    M-21: Add photos for buildings (#17)
    * M-21: Add photos for buildings
    
    * M-21: Add files
    
    * M-21: Deleted file photos
    
    * M-21: Add new migration

--- app/config/urls.py
+++ app/config/urls.py
@@ -14,10 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from django.contrib import admin
 from django.urls import include, path
 
+from config import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,3 +27,6 @@ urlpatterns = [
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='docs'),
     path('api/', include('realty.urls'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
