cc83ec32b25a101434ef3d8d12f7e30b2d091629
InnFil <inna_tretyakova_2015@mail.ru>
2024-03-25 15:27:49 +0400

    M-25: Add test for floors

---
+++ app/realty/domain/floor/tests/test_floor_selector.py
@@ -0,0 +1,12 @@
+from unittest import TestCase
+
+from realty.domain.floor.selectors import FloorDetailSelector
+from realty.models.floor import Floor
+
+
+class FloorTest(TestCase):
+    def test_floor_detail(self):
+        floor = Floor.objects.create(number=1, color='Бежевый', lighting=Floor.LUMINESCENT)
+
+        selector_result = FloorDetailSelector().floor_detail(pk=floor.id)
+        self.assertEqual(floor, selector_result)
