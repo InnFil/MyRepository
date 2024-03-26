92aeb9912f0c9f2c578b6fa86972df77f1858237
InnFil <156078099+InnFil@users.noreply.github.com>
2024-03-26 11:04:26 +0400

    M-25: Add test for floors (#22)
    * M-25: Add test for floors
    
    * M-25: Improved urls
    
    * M-25: Corrrected code

--- app/realty/domain/project/urls.py
+++ app/realty/domain/project/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from realty.domain.project.views import ProjectListAPI, ProjectDetailAPI
 
 urlpatterns = [
-    path('projects/', ProjectListAPI.as_view()),
-    path('projects/<int:pk>', ProjectDetailAPI.as_view()),
+    path('', ProjectListAPI.as_view()),
+    path('<int:pk>/', ProjectDetailAPI.as_view()),
 ]
